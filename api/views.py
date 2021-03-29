7bcbe96df399b09bafb827a379e6cdd44716cf95
Amy Gori <amydgori@gmail.com>
2021-03-29 13:45:34 -0400

    Restrict user permissions for ingredients by recipe

--- api/views.py
+++ api/views.py
@@ -33,19 +33,19 @@ class IngredientCreateView(CreateAPIView):
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
 
-#     def perform_create(self, serializer):
-#         recipe = serializer.validated_data['recipe']
-#         if self.request.user != recipe.user:
-#             raise PermissionDenied(
-#                 detail="You are not the owner of this recipe.")
-#         serializer.save()
+    def perform_create(self, serializer):
+        recipe = serializer.validated_data['recipe']
+        if self.request.user != recipe.user:
+            raise PermissionDenied(
+                detail="You are not the owner of this recipe.")
+        serializer.save()
 
 
 class IngredientDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
 
-    # def get_queryset(self):
-    #     # TODO need to filter down and make sure users can't update or delete
-    #     # ingredients on recipes that don't belong to them
-    #     return Ingredient.objects.filter(recipe__user=self.request.user)
+    def get_queryset(self):
+        # Filter down and make sure users can't update or delete
+        # ingredients on recipes that don't belong to them
+        return Ingredient.objects.filter(recipe__user=self.request.user)
